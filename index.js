bea13146793da5c480428db981904823f19b633a
Jourdan Templeton (AWARE) <jourdan.templeton@awaregroup.com>
2019-04-18 14:43:18 +0800

    Added additional logging and removed unused dependencies

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { EventHubClient, EventPosition } = require('@azure/event-hubs');
+const { EventHubClient } = require('@azure/event-hubs');
 const client = EventHubClient.createFromConnectionString(process.env["EVENTHUB_CONNECTION_STRING"], process.env["EVENTHUB_NAME"]);
  
 exports.handler = function(event, context, callback) {
   console.log('event', event);
   const delivery = await client.send(event);
+  console.log('result', delivery);
   callback(null, event);
 };
